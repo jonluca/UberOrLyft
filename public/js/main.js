6112d4bf356761485a430da067773385b3e67d6f
JonLuca DeCaro <jdecaro@usc.edu>
2017-03-16 18:29:55 -0700

    call for comparisons

--- public/js/main.js
+++ public/js/main.js
@@ -16,12 +16,12 @@ $(document).ready(function() {
   var startLong = "";
   var destLat = "";
   var destLong = "";
-
+  var counter = 0;
   //Google maps api to validate address and get lat/long
   function checkPrice(dest, initial) {
     // Get geocoder instance
     var geocoder = new google.maps.Geocoder();
-
+    counter = 1;
     // Geocode the address
     geocoder.geocode({
       'address': dest.value
@@ -34,11 +34,34 @@ $(document).ready(function() {
 
   }
 
+  function comparePrices() {
+    $.ajax({
+      method: 'POST',
+      url: "/search",
+      type: 'json',
+      data: {
+        startLatitude: startLat,
+        startLongitude: startLong,
+        endLatitude: destLat,
+        endLongitude: destLong
+      },
+      success: showResults,
+    });
+  }
+
+  function showResults(data, code, jqXHR) {
+
+  }
+
   function destinationResults(results, status) {
     if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
 
       destLat = results[0].geometry.location.lat();
       destLong = results[0].geometry.location.lng();
+      counter -= 1;
+      if (counter == 0) {
+        comparePrices();
+      }
     // show an error if it's not
     } else {
       console.log(status);
@@ -46,10 +69,16 @@ $(document).ready(function() {
     }
   }
 
+
+
   function startLocationResults(results, status) {
     if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
       startLat = results[0].geometry.location.lat();
       startLong = results[0].geometry.location.lng();
+      counter -= 1;
+      if (counter == 0) {
+        comparePrices();
+      }
     // show an error if it's not
     } else {
       console.log(status);
