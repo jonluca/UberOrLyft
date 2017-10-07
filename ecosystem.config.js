20bc11878c572b7b9e4558c863d6a40dd8a395c0
JonLuca DeCaro <jdecaro@usc.edu>
2017-10-07 13:13:58 -0700

    uol

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    /**
+     * Application configuration section
+     * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     */
+    apps: [
+        // First application
+        {
+            name: 'UberOrLyft',
+            script: 'app.js',
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+
+    ]
+};
